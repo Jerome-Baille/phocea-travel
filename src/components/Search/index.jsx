b3d95c8ee2e4d4e483934a2edbbdc3a454cdac75
Jerome-Baille <baille.jerome@gmail.com>
2022-07-11 17:36:07 +0200

    feat(search): add a search section
    A search bar and filters have been added to the main page

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,2 +1,56 @@
-const Search = () => {}
+import { HashLink as Link } from 'react-router-hash-link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInfo, faLocationDot, faMagnifyingGlass,  } from '@fortawesome/free-solid-svg-icons';
+import data from '../../assets/data.json';
+import iconMapping from '../iconMapping';
+
+const Search = () => {
+    return (
+      <section id="search">
+        <header>
+          <h1>Trouvez votre hébergement pour des vacances de rêve</h1>
+          <p>En plein centre ville ou en pleine nature</p>
+        </header>
+        <form method="get" action=".">
+          <span className="mapIconWrapper">
+          <FontAwesomeIcon icon={faLocationDot} />
+          </span>
+          <input type="search" id="citySearch" name="citySearch" placeholder="Marseille, France" aria-label="Enter the name of the city searched here" />
+          <button className="searchButton" disabled>
+            <span className="spanSearchButton">
+              Rechercher
+            </span>
+            <FontAwesomeIcon icon={faMagnifyingGlass} className="iconSearchButton"/>
+          </button>
+        </form>
+
+          <div className="filters-container">
+            <h3>Filtres</h3>
+            <div className="filtersSelection">
+
+            {Object.keys(data.filters).map((filter, index) => (
+              <div key={index} className="filters">
+                <Link to={data.filters[filter].url}>
+                  <div className="wrapFilterIcon">
+                    {iconMapping[data.filters[filter].icon]}
+                  </div>
+                  <span className="textFilter">{data.filters[filter].name}</span>
+                </Link>
+              </div>
+            ))}
+            </div>
+          </div>
+
+          <div className="infoWrapper"> 
+            <div className="wrapInfoIcon">
+              <FontAwesomeIcon icon={faInfo} />
+            </div>
+            <p>
+              Plus de 500 logements sont disponibles dans cette ville
+            </p>
+          </div>          
+        </section>
+    )
+}
+
 export default Search;
---
+++ src/components/iconMapping.jsx
@@ -0,0 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChildReaching, faDog, faHeart, faMoneyBillWave } from '@fortawesome/free-solid-svg-icons'
+
+const iconMapping = {
+    'faChildReaching': <FontAwesomeIcon icon={faChildReaching} />,
+    'faDog': <FontAwesomeIcon icon={faDog} />,
+    'faHeart': <FontAwesomeIcon icon={faHeart} />,
+    'faMoneyBillWave': <FontAwesomeIcon icon={faMoneyBillWave} />,
+}
+
+export default iconMapping;
