86b1e082740e9f8e76b1875e6411f023946c7029
Jerome-Baille <baille.jerome@gmail.com>
2022-07-14 17:38:51 +0200

    feat(forms): add form validation
    Added Formik to implement form validation and added material UI to display a dialog box informing
    the user that contact form was submitted

--- src/components/Help/ContactForm/index.jsx
+++ src/components/Help/ContactForm/index.jsx
@@ -1,23 +1,106 @@
+import React from 'react';
+import { useState } from "react";
 import { Link } from 'react-router-dom'
 
-const ContactForm = () => {
+import Dialog from "@mui/material/Dialog";
+import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+
+const ContactForm = () => {   
+  
+      // Open a modal to display success message
+      const [open, setOpen] = useState(false);
+
+      const handleClickToOpen = () => {
+        setOpen(true);
+      };
+      
+      const handleToClose = () => {
+        setOpen(false);
+      };
+
     return (
         <section id="Contact">
             <h2>Nous Contacter</h2>
 
             <div className="contact-form">
-                <form action="">
-                    <label htmlFor="name">Nom :</label>
-                        <input type="text" className="form-control" id="name" placeholder="Votre nom" />
+              <Formik
+                  initialValues={{ username: '', email: '', message: '' }}
+                  validate={values => {
+                    const errors = {};
+                    if (!values.username) {
+                      errors.username = 'Veuillez saisir votre nom';
+                    }
+                    if (!values.email) {
+                      errors.email = 'Une adresse e-mail est requise';
+                    } else if (
+                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                      errors.email = `L'adresse e-mail n'est pas valide`;
+                    }
+                    if (!values.message) {
+                      errors.message = 'Veuillez saisir votre message';
+                    }
+                    return errors;
+                  }}
+                  onSubmit={(values, 
+                    { setSubmitting, resetForm }) => {
+                      setTimeout(() => {
+                        handleClickToOpen();
+                        setSubmitting(false);
+                        resetForm();
+                      }, 400);
+                  }}
+                >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className="form-group">
+                    <label htmlFor="username">Nom :</label>
+                    <Field type="text" name="username" placeholder="Votre nom" />
+                    <ErrorMessage name="username" component="p" />
+                  </div>
+
+                  <div className="form-group">
                     <label htmlFor="email">E-mail:</label>
-                        <input type="email" className="form-control" id="email" placeholder="Votre email" />
+                    <Field type="email" name="email" placeholder="Votre adresse e-mail"/>
+                    <ErrorMessage name="email" component="p" />
+                  </div>
+
+                  <div className="form-group">
                     <label htmlFor="message">Message :</label>
-                        <textarea className="form-control" id="message" rows="3" placeholder="Votre message"></textarea>
-                    <input type="submit" className='main-btn' value="SEND" />
-                </form>
+                    <Field type="text" name="message" placeholder="Votre message" />
+                    <ErrorMessage name="message" component="p" />
+                  </div>
+                  <button type="submit" disabled={isSubmitting} className='main-btn form-btn'>
+                    Envoyer
+                  </button>
+                </Form>
+              )}
+              </Formik>
             </div>
 
             <Link to="/" className='main-btn'>Retour à l'accueil</Link>
+
+            <Dialog open={open} onClose={handleToClose}>
+                <DialogTitle>{"Message envoyé avec succès !"}</DialogTitle>
+                <DialogContent>
+                <Typography>
+                    Nous vous remercions pour votre message et nous y répondrons dans les plus brefs délais.
+                </Typography>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleToClose} 
+                        color="primary" autoFocus>
+                    Fermer
+                </Button>
+                </DialogActions>
+            </Dialog>
         </section>
     );
 }
--- src/components/Help/Submit-a-hotel/index.jsx
+++ src/components/Help/Submit-a-hotel/index.jsx
@@ -1,26 +1,116 @@
+import React from 'react';
+import { useState } from "react";
 import { Link } from 'react-router-dom'
 
-const SubmitAHotel = () => {
+import Dialog from "@mui/material/Dialog";
+import Typography from '@mui/material/Typography';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import Button from '@mui/material/Button';
+
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+
+const SubmitAHotel = () => {   
+  
+      // Open a modal to display success message
+      const [open, setOpen] = useState(false);
+
+      const handleClickToOpen = () => {
+        setOpen(true);
+      };
+      
+      const handleToClose = () => {
+        setOpen(false);
+      };
+
     return (
         <section id="Contact">
             <h2>Soumettre votre hotel</h2>
 
             <div className="contact-form">
-                <form action="">
+              <Formik
+                  initialValues={{ name: '', address: '', email: '', message: '' }}
+                  validate={values => {
+                    const errors = {};
+                    if (!values.name) {
+                      errors.name = 'Veuillez saisir le nom de votre hôtel';
+                    }
+                    if (!values.address) {
+                        errors.address = `Veuillez saisir l'adresse de votre hôtel`;
+                    }
+                    if (!values.email) {
+                      errors.email = 'Une adresse e-mail est requise';
+                    } else if (
+                      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+                    ) {
+                      errors.email = `L'adresse e-mail n'est pas valide`;
+                    }
+                    if (!values.message) {
+                      errors.message = 'Veuillez saisir votre message';
+                    }
+                    return errors;
+                  }}
+                  onSubmit={(values, 
+                    { setSubmitting, resetForm }) => {
+                      setTimeout(() => {
+                        handleClickToOpen();
+                        setSubmitting(false);
+                        resetForm();
+                      }, 400);
+                  }}
+                >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className="form-group">
                     <label htmlFor="name">Nom de l'hôtel :</label>
-                        <input type="text" className="form-control" id="name" placeholder="Nom de votre hôtel" />
-                    <label htmlFor="address">Adresse de l'hôtel :</label>
-                        <input type="text" className="form-control" id="address" placeholder="Adresse de votre hôtel" />
-                    <label htmlFor="email">E-mail :</label>
-                        <input type="email" className="form-control" id="email" placeholder="Votre email" />
+                    <Field type="text" name="name" placeholder="Nom de votre hôtel" />
+                    <ErrorMessage name="name" component="p" />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="address">Adresse de votre hôtel :</label>
+                    <Field type="text" name="address" placeholder="Adresse de votre hôtel" />
+                    <ErrorMessage name="address" component="p" />
+                  </div>
+
+                  <div className="form-group">
+                    <label htmlFor="email">E-mail:</label>
+                    <Field type="email" name="email" placeholder="Votre adresse e-mail"/>
+                    <ErrorMessage name="email" component="p" />
+                  </div>
+
+                  <div className="form-group">
                     <label htmlFor="message">Message :</label>
-                        <textarea className="form-control" id="message" rows="3" placeholder="Votre message"></textarea>
-                    <input type="submit" className='main-btn' value="SEND" />
-                </form>
+                    <Field type="text" name="message" placeholder="Votre message" />
+                    <ErrorMessage name="message" component="p" />
+                  </div>
+                  <button type="submit" disabled={isSubmitting} className='main-btn form-btn'>
+                    Envoyer
+                  </button>
+                </Form>
+              )}
+              </Formik>
             </div>
 
             <Link to="/" className='main-btn'>Retour à l'accueil</Link>
+
+            <Dialog open={open} onClose={handleToClose}>
+                <DialogTitle>{"Hôtel soumis avec succès !"}</DialogTitle>
+                <DialogContent>
+                <Typography>
+                    Nous vous remercions pour la soumission de votre hôtel et nous y répondrons dans les plus brefs délais.
+                </Typography>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleToClose} 
+                        color="primary" autoFocus>
+                    Fermer
+                </Button>
+                </DialogActions>
+            </Dialog>
         </section>
-    )
+    );
 }
 export default SubmitAHotel
